93b3c373a2fb95733ea4249e527ab3427c164e1c
Praise Yu <praiseyu@gmail.com>
2024-11-07 10:55:17 -0700

    Finish part 1.6 unicafe

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const increaseGood = () => {
+    setGood(good + 1);
+  };
+
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const increaseBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <>
+      <h2>give feedback</h2>
+      <Button handleClick={increaseGood} text="good" />
+      <Button handleClick={increaseNeutral} text="neutral" />
+      <Button handleClick={increaseBad} text="bad" />
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </>
+  );
+};
+
+export default App;
